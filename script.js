0bcf152ecc90d0e2f6651edd7ad945e68ad91eb2
Yuri Trondin <lopata@2ch.hk>
2020-10-18 18:02:03 +0500

    добавил в функционал ввод имени

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 const fraseField = document.querySelector(".app__frase");
 const question = document.querySelector(".app__header");
-const button = document.querySelector(".frase__generator");
+const fraseButton = document.querySelector(".frase__generator_button");
+const inputForm = document.querySelector(".name__input-container");
+const input = document.querySelector(".name__input_field");
+// const question = document.querySelector(".app__header");
+const nameButton = document.querySelector(".name__input_button");
+const app = document.querySelector(".app");
+
+let name = "";
 // console.log(frase);
 const fraseGenerate = () => {
-  question.classList.remove("hide");
   frase = doebFrases[Math.floor(Math.random() * doebFrases.length)];
   fraseField.textContent = frase.toLowerCase();
 };
 
-button.addEventListener("click", fraseGenerate);
+const nameInput = (evt) => {
+  evt.preventDefault();
+  name = input.value;
+  console.log(name);
+  changeName();
+  inputForm.classList.add("hide");
+  app.classList.remove("hide");
+  question.classList.remove("hide");
+  fraseGenerate();
+};
+
+function changeName() {
+  question.textContent = `Блядь, ${name}, какого хуя`;
+}
+
+fraseButton.addEventListener("click", fraseGenerate);
+inputForm.addEventListener("submit", nameInput);
